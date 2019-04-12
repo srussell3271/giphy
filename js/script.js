85ced8a90ab20fae2d556a3761ca8cf688ae50ba
Shannon Russell <s.russell3271@gmail.com>
2019-04-12 18:41:48 +0000

    giphy finalized

--- js/script.js
+++ js/script.js
@@ -4,8 +4,16 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+    
+    $('.gallery').empty();
+    var searchTerm = $("#search-term").val();
+    var url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC";
+    $.ajax({
+        url: url,
+        method: "GET",
+        success: function(response) {
+            var randomInt = Math.floor(Math.random() * response.data.length);
+            $('.gallery').append("<img src='" + response.data[randomInt].images.original.url + "'/>");
+        }
+    });
 });
-
